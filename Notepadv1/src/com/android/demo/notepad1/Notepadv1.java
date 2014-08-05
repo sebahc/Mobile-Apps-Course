f357134d6a626fec46ecf4a79738330f83affc5a
sebahc <sebaynil@gmail.com>
2014-08-05 12:59:17 -0300

    Ejercicio_Notepadv1

---
+++ Notepadv1/src/com/android/demo/notepad1/Notepadv1.java
@@ -0,0 +1,28 @@
+package com.android.demo.notepad1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Notepadv1 extends Activity {
+    private int mNoteNumber = 1;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        return super.onOptionsItemSelected(item);
+    }
+}
